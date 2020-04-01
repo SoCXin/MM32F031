7eebeb51db356007030362a7951c51f73c7aea3b
qitas <qitas@qitas.cn>
2020-04-01 19:00:55 +0800

    fix

--- src/MBF031C6T/BLINK/IOKey3/USER/main.c
+++ src/MBF031C6T/BLINK/IOKey3/USER/main.c
@@ -6,17 +6,8 @@
 * @date     15/05/2019
 * @brief
 ******************************************************************************
-* @copy
-*
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-* TIME. AS A RESULT, MindMotion SHALL NOT BE HELD LIABLE FOR ANY
-* DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-* FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-* CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*
-* <h2><center>&copy; COPYRIGHT 2019 MindMotion</center></h2>
 */
+
 #include "HAL_device.h"
 #include "HAL_conf.h"
 #include "stdio.h"
@@ -203,6 +194,7 @@ static u16 KEY1BitBuf = 0x0;
 static u16 KEY2BitBuf = 0x0;
 static u16 KEY3BitBuf = 0x0;
 static u16 KEY4BitBuf = 0x0;
+
 void Key_Scan(void)
 {
     /*¼ì²âSW1ÊÇ·ñ°´ÏÂ */
