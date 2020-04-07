5534e46852d488ab7a95721f8581108c804632f2
qitas <qitas@qitas.cn>
2020-04-07 10:29:30 +0800

    fix pro adc

--- src/F031C6T/ADC/ADC_BasicExample/USER/main.c
+++ src/F031C6T/ADC/ADC_BasicExample/USER/main.c
@@ -1,21 +1,11 @@
 /**
 ******************************************************************************
 * @file     main.c
-* @author   AE team
-* @version  V1.1.1
-* @date     15/05/2019
+* @author   Qitas
+* @version  V1.0.0
+* @date     02/04/2020
 * @brief
 ******************************************************************************
-* @copy
-*
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-* TIME. AS A RESULT, MindMotion SHALL NOT BE HELD LIABLE FOR ANY
-* DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-* FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-* CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*
-* <h2><center>&copy; COPYRIGHT 2019 MindMotion</center></h2>
 */
 
 #include "HAL_device.h"
@@ -47,17 +37,26 @@ u16 Get_Adc_Average(uint8_t times);
 extern u32 SystemCoreClock;
 
 void delay_init(void);
-
-
+void TimingDelay_Decrement(void);
+static __IO uint32_t TimingDelay;
 void delay_ms(__IO uint32_t nTime);
 
+char printBuf[100];
 
-void TimingDelay_Decrement(void);
 
-static __IO uint32_t TimingDelay;
+void uart_initwBaudRate(u32 bound);
 
-u16 ADCVAL;
-float fValue;
+void UartSendGroup(u8* buf, u16 len)
+{
+    while(len--)
+    {
+        // UART_SendData(UART1, *buf++);
+        UART1->TDR = *buf++ ;
+        while(!UART_GetFlagStatus(UART1, UART_FLAG_TXEPT));
+        // while((UART1->CSR & UART_FLAG_TXEPT) == (uint16_t)RESET);
+    } 
+}
+
 /********************************************************************************************************
 **函数信息 ：main(void)
 **功能描述 ：
@@ -67,20 +66,55 @@ float fValue;
 ********************************************************************************************************/
 int main(void)
 {
-
+    u16 ADCVAL;
+    float fValue;
     delay_init();
-
+    uart_initwBaudRate(9600);
     ADCConfig(ADCch0); //use PA0
-    while(1) {
+    while(1) 
+    {
         ADCVAL = Get_Adc_Average(5);
         fValue = ((float)ADCVAL / 4095) * 3.3; //use 3.3V as VDD
         //fValue = (((float)ADCVAL)/4095)*5; //use 5V as VDD
-
-        delay_ms(200);
+        UartSendGroup((u8*)printBuf, sprintf(printBuf, "ADC[%d]:%f\r\n",ADCVAL,fValue));
+        delay_ms(400);
     }
 
 }
+void uart_initwBaudRate(u32 bound)
+{
+    //GPIO端口设置
+    GPIO_InitTypeDef GPIO_InitStructure;
+    UART_InitTypeDef UART_InitStructure;
 
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_UART1, ENABLE);                       //使能UART1时钟
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);                         //开启GPIOA时钟
+    //UART 初始化设置
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_1);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_1);
+
+    UART_InitStructure.UART_BaudRate = bound;                                   //串口波特率
+    UART_InitStructure.UART_WordLength = UART_WordLength_8b;                    //字长为8位数据格式
+    UART_InitStructure.UART_StopBits = UART_StopBits_1;                         //一个停止位
+    UART_InitStructure.UART_Parity = UART_Parity_No;                            //无奇偶校验位
+    UART_InitStructure.UART_HardwareFlowControl = UART_HardwareFlowControl_None;//无硬件数据流控制
+    UART_InitStructure.UART_Mode = UART_Mode_Rx | UART_Mode_Tx;	                //收发模式
+
+    UART_Init(UART1, &UART_InitStructure);                                      //初始化串口1
+    UART_Cmd(UART1, ENABLE);                                                    //使能串口1
+
+    //UART1_TX   GPIOA.9
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;                                   //PA.9
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;                             //复用推挽输出
+    GPIO_Init(GPIOA, &GPIO_InitStructure);                                      //初始化GPIOA.9
+
+    //UART1_RX	  GPIOA.10初始化
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;                                  //PA10
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;                       //浮空输入
+    GPIO_Init(GPIOA, &GPIO_InitStructure);                                      //初始化GPIOA.10
+
+}
 /********************************************************************************************************
 **函数信息 ：delay_init(void)
 **功能描述 ：systick延时函数初始化
@@ -277,17 +311,10 @@ void delay_ms(__IO uint32_t nTime)
     while(TimingDelay != 0);
 }
 
-/**
-* @}
-*/
 
 /**
 * @}
 */
 
-/**
-* @}
-*/
-
-/*-------------------------(C) COPYRIGHT 2019 MindMotion ----------------------*/
+/*-------------------------(C) COPYRIGHT 2020 QITAS ----------------------*/
 
